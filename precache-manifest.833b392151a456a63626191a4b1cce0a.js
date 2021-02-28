8ab33d2a2bb38c24bd4850ae00ff4036fd2cdd7c
sajith-noetic <sajith.wijerathne@wearenoetic.com>
2021-02-28 11:40:43 +0530

    Updates

---
+++ precache-manifest.833b392151a456a63626191a4b1cce0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1013a614c8bc15af340e8501cc6022a",
+    "url": "/testar-web/index.html"
+  },
+  {
+    "revision": "2a218f34c5424df2624d",
+    "url": "/testar-web/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bf657a0f7a1d16ac3193",
+    "url": "/testar-web/static/js/2.6e46acf4.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/testar-web/static/js/2.6e46acf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a218f34c5424df2624d",
+    "url": "/testar-web/static/js/main.f5fd4e1a.chunk.js"
+  },
+  {
+    "revision": "ca4e089e85caf9f97add",
+    "url": "/testar-web/static/js/runtime-main.a5daa279.js"
+  }
+]);
